54a03624b4b9d4dffee5582183f32e478f69285c
sundus <s.alghernas1414@hotmail.com>
2020-07-02 00:02:05 +0300

    task 3

--- task_3.php
+++ task_3.php
@@ -6,9 +6,9 @@
 <html>
     <head>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+     
     </head>
     <body>
-
         <div class="controller pl-5 pr-5">
             <br><br>
             <div class="buttons text-center" style="text-align:center" role="group">
@@ -25,10 +25,9 @@
                     </div>
 
                     <div class="form-group">
-                        <label>Left</label>
-                        <input style="width: 100%;;" type="number" class="form-control" name="left">
+                        <label>Backward</label>
+                        <input style="width: 100%;" type="number" class="form-control" name="backward">
                     </div>
-
                     <button style="float: center; margin:5px;" type="submit"  name="save"  class="btn btn-success">Save</button>
                     <button style="float: center; margin:5px;" type="submit"  name="start"  class="btn btn-secondary">Start</button>
                     <button style="float: center; margin:5px;" type="submit"  name="delete"  class="btn btn-danger">Delete</button>   
@@ -44,11 +43,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $right = (int) $_POST['right'] ;
     $forward = (int) $_POST['forward'];
-    $left = (int) $_POST['left'];
+    $backward = (int) $_POST['backward'];
 
     // Save data in DB 
     if (isset($_POST["save"])){
-        $sql =  "INSERT INTO `roport_info`(`Right`, `Forward`, `Left`) VALUES ($right, $forward, $left)";
+        $sql =  "INSERT INTO `roport_info`(`Right`, `Forward`, `Backward`) VALUES ($right, $forward, $backward)";
 
         if(mysqli_query($mysqli, $sql)){
             echo "";
@@ -56,13 +55,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             echo "ERROR: Could not able to execute $sql. " . mysqli_error($mysqli);
         }
 
-        $result = "SELECT `Right`, `Left`, `Forward` FROM `roport_info`";
+        $result = "SELECT `Right`, `Backward`, `Forward` FROM `roport_info`";
         echo "<center><h2>Robot Tracking</h2>";
         echo "<center><table border='1'>
         <tr>
         <th>Right</th>
         <th>Forward</th>
-        <th>Left</th>
+        <th>Backward</th>
         </tr>";
         if ($result = $mysqli->query($result)) {
             while($row = $result->fetch_assoc())
@@ -70,7 +69,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             echo "<tr>";
             echo "<td>" . $row['Right'] . "</td>";
             echo "<td>" . $row['Forward'] . "</td>";
-            echo "<td>" . $row['Left'] . "</td>";
+            echo "<td>" . $row['Backward'] . "</td>";
             echo "</tr>";
             }
             echo "</table>";
@@ -81,26 +80,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     elseif(isset($_POST["delete"])){
         $right = '';
         $forward = '';
-        $left = '';
+        $backward = '';
     } 
     // Start to draw line
     elseif(isset($_POST["start"])){
 
-        //Forward
-        // echo "<center><h2>Robot Path</h2>";
-        // echo '<center><svg height="210" width="500">
-        //         <line x1="0" y1="0" x2="0" y2="' .$forward.'" style="stroke:rgb(255,0,0);stroke-width:2"/>
-        //       </svg>';
-
-        //Right
-        // echo '<center><svg height="210" width="500">
-        //     <line x1=""' .$forward.'"" y1="" x2=""' .$right.'"" y2=" " style="stroke:rgb(255,0,0);stroke-width:2"/>
-        //     </svg>';
-
-        // //Left
-        // echo '<svg height="210" width="500">
-        // <line x1="0" y1="0" x2="0" y2="' .$forward.'" style="stroke:rgb(255,0,0);stroke-width:2"/>
-        // </svg>';
+        echo "<center><h2>Robot Path</h2>";
+        echo '<center><svg height="300" width="300">
+            <path d="M 0,0 L 0,'.$forward.' h '.$right.',0 z stroke="green" stroke-width:2" />
+            </svg>';
     }
 }
 
